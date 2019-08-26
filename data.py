2ba34e247241529d2f63244a595e71bf41b3fe55
George Luke <george.luke92@gmail.com>
2019-08-26 12:00:13 -0500

    updated app.py cursor

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+
+    articles = [
+        {
+            'id': 1,
+            'title': 'Why Poop?',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit expedita, maxime nihil, tenetur iure cumque magni optio alias deserunt soluta, facilis incidunt dolores necessitatibus doloremque recusandae blanditiis saepe earum voluptate!',
+            'author': 'George Luke',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Those eyes of green and forest wood',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit expedita, maxime nihil, tenetur iure cumque magni optio alias deserunt soluta, facilis incidunt dolores necessitatibus doloremque recusandae blanditiis saepe earum voluptate!',
+            'author': 'Joelle Kruger',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Those eyes of green and forest wood',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit expedita, maxime nihil, tenetur iure cumque magni optio alias deserunt soluta, facilis incidunt dolores necessitatibus doloremque recusandae blanditiis saepe earum voluptate!',
+            'author': 'Joelle Kruger',
+            'create_date': '04-25-2017'
+        }
+    ]
+    return articles
